bf9806dcdbd0283adbea0a2ff7755c069a7afd3a
Sinthujan <sinthujan2005@gmail.com>
2026-05-19 20:23:59 -0400

    Created fetch.py: added error handling, os.path.exist check, and move constants to top

---
+++ src/fetch.py
@@ -0,0 +1,30 @@
+import requests
+import pandas as pd
+import os
+import logging
+
+log = logging.getLogger(__name__)
+URL = 'https://open.canada.ca/data/dataset/ea639e28-c0fc-48bf-b5dd-b8899bd43072/resource/e8c27948-6a40-452b-8d7d-2e1b799ca8aa/download/job-bank-open-data-all-job-postings-en-feb2026.csv'
+DATAPATH = "data/raw_postings.csv"
+
+def fetch_data():
+    """
+    * Downloads the Job Bank CSV using requests.get()
+    * Saves it to data/raw_postings.csv
+    * Loads it into a DataFrame with the correct delimiter and encoding
+    * Returns the DataFrame
+    """
+    
+    if not os.path.exists(DATAPATH):
+        try:
+            response = requests.get(URL, timeout=30)
+            response.raise_for_status()
+            with open(DATAPATH, "wb") as f:
+                f.write(response.content)
+        except requests.RequestException as e:
+            log.error(f"Failed to download data: {e}")
+            return None
+            
+    df = pd.read_csv(DATAPATH, delimiter="\t", encoding="utf-16")
+    
+    return df
